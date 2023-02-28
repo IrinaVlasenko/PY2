acd008516a3771d6612c6529279bbc6728c33981
IrinaVlasenko <115364449+IrinaVlasenko@users.noreply.github.com>
2023-02-28 17:54:37 +0300

    ЛР_4_испр

--- Лабораторная работа 4/main.py
+++ Лабораторная работа 4/main.py
@@ -1,47 +1,51 @@
-if __name__ == "__main__":
-    class Car:
-        """ Базовый класс автомобили. """
+class Car:
+    """ Базовый класс автомобили. """
 
-        def __init__(self, brand: str, mileage: float, price: float):
-            self.brand = brand
-            self.mileage = mileage
-            self.price = price
+    def __init__(self, brand: str, mileage: float, price: float):
+        self.brand = brand
+        self.mileage = mileage
+        self.price = price
 
-        def __str__(self) -> str:
-            """Магический метод, возвращающий строковый объект"""
-            return f"Автомобиль {self.brand}. Пробег {self.mileage}. Цена {self.price}"
+    def __str__(self) -> str:
+        """Магический метод, возвращающий строковый объект"""
+        return f"Автомобиль {self.brand}. Пробег {self.mileage}. Цена {self.price}"
 
-        def __repr__(self) -> str:
-            """Магический метод, возвращающий строковый объект"""
-            return f"{self.__class__.__name__}(brand={self.brand!r}, mileage={self.mileage!r}, price={self.price!r})"
+    def __repr__(self) -> str:
+        """Магический метод, возвращающий строковый объект"""
+        return f"{self.__class__.__name__}(brand={self.brand!r}, mileage={self.mileage!r}, price={self.price!r})"
 
-        def mileage_in_miles(self) -> float:
-            """Перевод пробега из км в мили"""
-            return self.mileage * 0.621
+    def mileage_in_miles(self) -> float:
+        """Перевод пробега из км в мили"""
+        return self.mileage * 0.621
 
-        def price_with_discount(self) -> float:
-            """Цена со скидкой"""
-            ...
-
-
-    class TruckCar(Car):
-        """Дочерний класс Грузовой автомобиль"""
-
-        def __init__(self, brand: str, mileage: float, price: float, capacity: float):
-            super().__init__(brand, mileage, price)  # Вызывает метод родительского класса
-            self.capacity = capacity
-
-        def __str__(self) -> str:
-            """Перегрузка метода, чтобы выводил Грузовой автомобиль и аргумент Допустимая нагрузка"""
-            return f"Грузовой автомобиль {self.brand}. Пробег {self.mileage}. Цена {self.price}. Допустимая нагрузка {self.capacity}"
-
-        def __repr__(self) -> str:
-            """Перегрузка метода, чтобы выводил аргумент Допустимая нагрузка"""
-            return f"{self.__class__.__name__}(brand={self.brand!r}, mileage={self.mileage!r}, price={self.price!r}, capacity={self.capacity!r})"
-
-        def price_with_discount(self) -> float:
-            """Цена со скидкой.
-            Пусть скидка зависит от грузоподъемности, тогда перегружаем метод, чтобы задать свою реализацию в классе"""
+    def price_with_discount(self) -> float:
+        """Цена со скидкой"""
         ...
 
-    pass
+
+class TruckCar(Car):
+    """Дочерний класс Грузовой автомобиль"""
+
+    def __init__(self, brand: str, mileage: float, price: float, capacity: float):
+        super().__init__(brand, mileage, price)  # Вызывает метод родительского класса
+        self.capacity = capacity
+
+    def __str__(self) -> str:
+        """Перегрузка метода, чтобы выводил Грузовой автомобиль и аргумент Допустимая нагрузка"""
+        return f"Грузовой автомобиль {self.brand}. Пробег {self.mileage}. Цена {self.price}. Допустимая нагрузка {self.capacity}"
+
+    def __repr__(self) -> str:
+        """Перегрузка метода, чтобы выводил аргумент Допустимая нагрузка"""
+        return f"{self.__class__.__name__}(brand={self.brand!r}, mileage={self.mileage!r}, price={self.price!r}, capacity={self.capacity!r})"
+
+    def price_with_discount(self) -> float:
+        """Цена со скидкой.
+        Пусть скидка зависит от грузоподъемности, тогда перегружаем метод, чтобы задать свою реализацию в классе"""
+    ...
+
+if __name__ == "__main__":
+    car_1 = Car("Audi", 20, 5000.6)
+    truck_car_1 = TruckCar("Opel", 30.6, 500, 600)
+    print(car_1)
+    print(truck_car_1)
+pass
